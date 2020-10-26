3145a8657f4c0cab8a45591074f1d78d016785b1
kceron <karemiceron@gmail.com>
2020-10-26 19:39:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
- function largestSubarraySum(arr){
+function largestSubarraySum(arr) {
+  let curr_max = 0,
+    maxSum = 0;
 
-    let curr_max = 0, maxSum = 0;
-  
-    for(let i = 0; i < arr.length; i++){  
-      curr_max = Math.max(0, curr_max + arr[i]);
-      maxSum = Math.max(curr_max, maxSum);
-    }
-    return maxSum;
+  for (let i = 0; i < arr.length; i++) {
+    curr_max = Math.max(0, curr_max + arr[i]);
+    maxSum = Math.max(curr_max, maxSum);
   }
+  return maxSum;
+}
