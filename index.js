d8e2e439d6ed95d727dd355b1bfbb1fd91468789
kceron <karemiceron@gmail.com>
2020-10-26 19:39:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function largestSubarraySum(arr) {
-  let curr_max = 0,
-    maxSum = 0;
+  let curr_max = 0, maxSum = 0;
 
   for (let i = 0; i < arr.length; i++) {
     curr_max = Math.max(0, curr_max + arr[i]);
